468cf2f04ef23a70fd454476761bb2623afd66fc
Samina Chhatwal <csaminak@gmail.com>
2016-08-16 15:49:38 -0400

    added the calc popularity functionality

--- src/app/js/repos.controller.js
+++ src/app/js/repos.controller.js
@@ -15,17 +15,30 @@
 
 
         function getRepos(username) {
-            console.log(username);
             if(!username) {
                 return;
             }
             repos.getUserRepos(username)
                 .then(function(repos) {
                     console.log(repos);
+                    repos.forEach(function(repo) {
+                        repo.popularity = calcPopularity(repo);
+                    });
                     that.allRepos = repos;
                     that.username = '';
                 });
         }
+
+        /**
+         * Uses an individual repo's properties to calculate it's popularity
+         * @param  {Object }    repo    contains properties to calc popularity
+         * @return {Number}             the calculaed popularity of the repo
+         */
+        function calcPopularity(repo) {
+            return (repo.stargazers_count + (2*repo.forks_count) + (repo.open_issues_count/2));
+        }
+
+
 
 
     }
